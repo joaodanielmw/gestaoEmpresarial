02ad4d27ed5006140535f863f6e0813e9391c79f
João Daniel M Weng <joaomweng@gmail.com>
2025-07-30 22:15:50 -0300

    Criação de rotas para o projeto

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () { return 'Home'; })->name('index');
+
+Route::get('/produtos', function () {return 'Produtos';});
+
+Route::get('/produtos/{id}', function (Int $id) {return 'Produtos id -> '.$id;})->where('id', '[0-9]+');
+
+Route::get('/cadastro', function () {return 'Cadastro';});
+
+Route::get('/login', function () {return 'Login';});
+
+Route::get('/sobre', function () {return 'Sobre';});
+
+Route::prefix('/admin')->group(function(){
+    Route::get('/login', function () {return 'Admin Login';});
+
+    Route::get('/dashboard', function () {return 'Admin Dashboard';});
+
+    Route::get('/clientes', function () {return 'Admin Clientes';});
+
+    Route::get('/clientes/{id?}', function (Int $id) {return 'Admin Clentes id -> '.$id;})->where('id', '[0-9]+');
+
+    Route::get('/fornecedores', function () {return 'Admin Fornecedores';});
+
+    Route::get('/produtos', function () {return 'Admin Produtos';});
+
+    Route::get('/produto/{slug}', function (String $slug) {return 'Admin Produtos slug -> '.$slug;})->where('slug', '[A-Za-z]+');
+});
+
+Route::fallback(function(){
+    return 'Rota não encontrada. <a href="'. route('index') .'">Voltar</a>';
+});
